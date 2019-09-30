0df003859a0d74de1071acd4a6e9c867ff3332b9
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-30 12:21:04 -0700

    Installed express and imported it into index.js. Project setup completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express'); //import express package
+
+const dbModel = require('./data/db');
+
+const server = express(); //creates the server
+
+
+//watch for connections on port 8000
+const port = 8000;
+server.listen(port, () => console.log('\nserver running\n'))
