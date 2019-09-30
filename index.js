c991172b4db6d66155df4708ff87bc5bd02b3af5
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-30 15:30:59 -0700

    get user by id request completed successfully

--- index.js
+++ index.js
@@ -47,6 +47,27 @@ server.get('/api/users', (req,res) => {
         res.status(500).json({ error: "The users information could not be retrieved." });
       });
 })
-//watch for connections on port 8000
+
+//get by id
+
+server.get('/api/users/:id', (req, res) => {
+    dbModel
+    .findById(req.params.id)
+    .then(db => {
+        if (db) {
+            res.status(200).json(db);
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        }
+       
+    })
+    .catch(() => {
+        res.status(500).json({error: "The user information could not be retrieved."})
+    })
+});
+
+
+
+//watch for connections on port 5000
 const port = 5000;
-server.listen(port, () => console.log(`\n***server running***\n`))
+server.listen(port, () => console.log(`\n*** API on port ${port} ***\n`))
